7ea83dd1a605558b16f54123239b54048a9c5cfa
Kipp Cannon <kcannon@cita.utoronto.ca>
2022-09-05 00:35:48 +0900

    loopback.py:  hack off-by-1 buffer bug
    - this puts in a stupid hack to fix the problem of restarting after an aborted loop-back test that leaves the input and output buffers 1 buffer out of step

--- utils/loopback.py
+++ utils/loopback.py
@@ -7,13 +7,18 @@ device = usb.core.find(idVendor = 0x04b4, idProduct = 0x1004)
 
 #device.set_configuration()
 
+def read_response():
+	n = device.read(0x86, buf)
+	msg = buf[:n].tobytes().decode("ascii")
+	print(msg)
+	return int(msg, 16)
+
 while True:
 	x = random.randint(0, 0xffff)
 	# end-point 2 has address 0x02
 	# end-point 6 has address 0x86
 	device.write(0x02, ("E%04X\n" % x).encode("ascii"))
-	n = device.read(0x86, buf)
-	msg = buf[:n].tobytes().decode("ascii")
-	print(msg)
-	y = int(msg, 16)
+	y = read_response()
+	if y != x:
+		y = read_response()
 	assert y == x
