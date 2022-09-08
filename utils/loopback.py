b9b749a678c07e524b5f8d7271b555889fa5ce80
Kipp Cannon <kcannon@cita.utoronto.ca>
2022-09-09 00:06:19 +0900

    loopback.py:  clean up
    - now that this is working reliably, we can make it prettier

--- utils/loopback.py
+++ utils/loopback.py
@@ -1,4 +1,5 @@
 import random
+from tqdm import tqdm
 import usb.core
 
 buf = usb.core.array.array("B", (0,) * 512)
@@ -8,15 +9,14 @@ device = usb.core.find(idVendor = 0x04b4, idProduct = 0x1004)
 device.set_configuration()
 
 def read_response():
+	# in end-point 6 has address 0x86
 	n = device.read(0x86, buf)
 	msg = buf[:n].tobytes().decode("ascii")
-	print(msg)
 	return int(msg, 16)
 
-while True:
+for i in tqdm(range(100000), desc = "running loopback test"):
 	x = random.randint(0, 0xffff)
-	# end-point 2 has address 0x02
-	# end-point 6 has address 0x86
+	# out end-point 2 has address 0x02
 	device.write(0x02, ("E%04X\n" % x).encode("ascii"))
 	y = read_response()
 	assert y == x
