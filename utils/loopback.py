ad33df5f5400fe3611255f96e11cbd9c4dd3f4d1
Kipp Cannon <kcannon@cita.utoronto.ca>
2022-09-15 23:15:23 +0900

    loopback.py:  add fast loopback test
    - exercises bulk command queue feature

--- utils/loopback.py
+++ utils/loopback.py
@@ -1,14 +1,22 @@
-from allpro88 import allpro88
+import allpro88
 import random
 from tqdm import tqdm
 
-allpro88 = allpro88()
+programmer = allpro88.allpro88()
 
 for i in tqdm(range(100000), desc = "loopback test"):
 	snd = random.randint(0, 0xffff)
-	rcv, = allpro88.write_command("E", snd)
+	rcv, = programmer.write_command("E", snd)
 	assert rcv == snd
+
+for i in tqdm(range(320000), desc = "fast loopback test"):
+	snd = random.randint(0, 0xffff)
+	programmer.push(allpro88.command("E", snd))
+	if len(programmer.out_queue) >= 32:
+		programmer.commit()
+		for cmd in programmer.pop_all():
+			assert cmd.response == cmd.addr
 
 for i in tqdm(range(100000), desc = "port read speed test"):
 	# device address 0x0280 is the socket board ID
-	allpro88.write_command("?", 0x0280)
+	programmer.write_command("?", 0x0280)
