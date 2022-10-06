0ac1d0cbd04bb657a9a69ea2825b704df17dab63
Kipp Cannon <kcannon@cita.utoronto.ca>
2022-10-06 23:08:42 +0900

    save vadj_ramp.py script

---
+++ utils/vadj_ramp.py
@@ -0,0 +1,30 @@
+import allpro88
+import sys
+import time
+from tqdm import tqdm
+
+programmer = allpro88.allpro88()
+
+print("system ID = 0x%X\nsocket adapter ID = 0x%X" % (programmer.system_id, programmer.socket_id))
+
+# PCR enable
+programmer.pcr_enable = True
+
+with tqdm(desc = "VADJ", total = 255, mininterval = 0.) as progress:
+	def set_vadj(vadj):
+		programmer.vadj = progress.n = vadj
+		progress.refresh()
+
+	try:
+		for i in range(10):
+			for vadj in range(256):
+				set_vadj(vadj)
+				time.sleep(10. / 256)
+			for vadj in range(255, -1, -1):
+				set_vadj(vadj)
+				time.sleep(10. / 256)
+	except KeyboardInterrupt:
+		set_vadj(0)
+
+# PCR disable
+programmer.pcr_enable = False
