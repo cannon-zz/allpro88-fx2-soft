44605744cb578cb4fb2b39e3d8afa6319b25316a
Kipp Cannon <kcannon@cita.utoronto.ca>
2022-10-06 23:11:05 +0900

    blink.py:  new version
    - for the "busy blink" version, just blink the IDLE led, don't turn the power supplies on and off
    - port to new property-based interface to dac registers
    - remove all the experimental code, go back to what should be working while I continue debugging the problems.

--- utils/blink.py
+++ utils/blink.py
@@ -6,54 +6,42 @@ programmer = allpro88.allpro88()
 
 print("system ID = 0x%X\nsocket adapter ID = 0x%X" % (programmer.system_id, programmer.socket_id))
 
-def blink_busy():
-	for i in tqdm(range(10), desc = "blink BUSY LED"):
-		programmer.write_command("=", 0x030c, 0x03)
+def blink_idle():
+	for i in tqdm(range(10), desc = "blink IDLE LED"):
+		programmer.write_command("=", 0x030c, 0x02)
 		time.sleep(0.5)
 		programmer.write_command("=", 0x030c, 0x00)
 		time.sleep(0.5)
 
 def blink_zif_pin1():
 	# VADJ = 35 (5 V)
-	#programmer.write_command("=", 0x0302, 35)
-	programmer.write_command("=", 0x0302, 255)
+	programmer.vadj = 35
 	# VTST = 26 (3 V), ITST = 5 (5 mA)
-	programmer.write_command("=", 0x0386, 26)
-	programmer.write_command("=", 0x0387, 5)
-	# VPUL = max
-	programmer.write_command("=", 0x0305, 255)
-	programmer.write_command("=", 0x0308, 0)
-	# PCR enable
-	programmer.write_command("=", 0x030c, 0x03)
+	programmer.vtst = 26
+	programmer.itst = 5
 	# set all pins to ground
 	for pin in range(88):
-		programmer.write_command("=", programmer.pin_addr(pin), 0x01)
-	# set pin 40 DAC to 35 (3 V)
-	pin = 40
-	programmer.write_command("=", programmer.pin_addr(pin) + 3, 255)
-	programmer.write_command("=", 0x0308, 0)
+		programmer.write_command("=", programmer.pin_addr(pin), allpro88.PINCON.GND)
+	# PCR enable
+	programmer.pcr_enable = True
 	# toggle pin 40 (zif socket pin 1) between VTST and ground
-	for i in tqdm(range(10), desc = "blink BUSY LED"):
-		#programmer.write_command("=", programmer.pin_addr(pin), 0x04)
-		programmer.write_command("=", programmer.pin_addr(pin), 0x10)
+	pin = 40
+	for i in tqdm(range(10), desc = "blink ZIF pin 1"):
+		programmer.write_command("=", programmer.pin_addr(pin), allpro88.PINCON.VTST)
 		time.sleep(0.5)
-		programmer.write_command("=", programmer.pin_addr(pin), 0x01)
+		programmer.write_command("=", programmer.pin_addr(pin), allpro88.PINCON.GND)
 		time.sleep(0.5)
 	# set all pins to disable
 	for pin in range(88):
-		programmer.write_command("=", programmer.pin_addr(pin), 0)
-	# set pin 40 DAC and VPUL to 0
-	programmer.write_command("=", programmer.pin_addr(pin) + 3, 0)
-	programmer.write_command("=", 0x0305, 0)
-	programmer.write_command("=", 0x0308, 0)
+		programmer.write_command("=", programmer.pin_addr(pin), allpro88.PINCON.DISABLE)
 	# VTST = 0, ITST = 0
-	programmer.write_command("=", 0x0386, 0)
-	programmer.write_command("=", 0x0387, 0)
+	programmer.vtst = 0
+	programmer.itst = 0
 	# VADJ = 0
-	programmer.write_command("=", 0x0302, 0)
+	programmer.vadj = 0
 	# PCR disable
-	programmer.write_command("=", 0x030c, 0)
+	programmer.pcr_enable = False
 
-#blink_busy()
+#blink_idle()
 
 blink_zif_pin1()
