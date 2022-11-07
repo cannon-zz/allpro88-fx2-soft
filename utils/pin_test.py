49774f01cecbff45bb936ed1b1ec5b3f27fcbf3f
Kipp Cannon <kcannon@cita.utoronto.ca>
2022-11-07 23:37:28 +0900

    pin_test:  report ZIF48 pin numbers

--- utils/pin_test.py
+++ utils/pin_test.py
@@ -204,6 +204,10 @@ with allpro88.allpro88() as programmer:
 
 	for channel in range(48):
 		test_suite = channel_driver_test_suite(programmer, programmer.channel[channel])
+		try:
+			print("channel %d --> ZIF48 pin %d" % (channel, programmer.socket_module.channel_lookup("ZIF48", channel)))
+		except KeyError:
+			print("channel %d --> ZIF48 no connection" % channel)
 
 		test_suite.test_logich()
 
