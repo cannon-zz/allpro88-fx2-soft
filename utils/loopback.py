719f64a9acca25fb7a84b666beb58fb6ec1306e8
Kipp Cannon <kcannon@cita.utoronto.ca>
2022-11-09 23:28:05 +0900

    loopback:  increase iterations of voltage read speed test

--- utils/loopback.py
+++ utils/loopback.py
@@ -22,7 +22,7 @@ for i in tqdm(range(100000), desc = "port read speed test"):
 	programmer.write_command("?", 0x0280)
 
 programmer.pcr_enable = True
-for i in tqdm(range(10000), desc = "voltage read speed test"):
+for i in tqdm(range(30000), desc = "voltage read speed test"):
 	# my unit only has 48 channels installed
 	programmer.channel[random.randint(0, 47)].measure_v()
 programmer.pcr_enable = False
