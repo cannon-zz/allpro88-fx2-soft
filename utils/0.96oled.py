44dd6116319365eff3693cf8dbd526a2e3cdb491
Kipp Cannon <kcannon@cita.utoronto.ca>
2022-11-25 10:15:44 +0900

    0.96oled:  update for deprecation of .textsize()
    - but what the pillow documentation says is the replacement is non-functional.  wft.  so I added cross-hairs to confirm the requested anchor point is in the correct location.  I'm going to leave it, maybe one day it magically starts working.

--- utils/0.96oled.py
+++ utils/0.96oled.py
@@ -71,7 +71,7 @@ with allpro88.allpro88(calibration_file = open("calibration.dat")) as programmer
 		display = ssd1306(i2c(bus = device.i2c, port = None, address = device.device_id << 1 | device.address))
 		display.contrast(1)
 		with canvas(display, dither = True) as draw:
-			message = "HELLO WORLD"
-			text_size = draw.textsize(message)
-			draw.text(((display.width - text_size[0]) // 2, (display.height - text_size[1]) // 2), message, fill = "white")
+			draw.line(((0, display.height // 2), (display.width, display.height // 2)), "gray")
+			draw.line(((display.width // 2, 0), (display.width // 2, display.height)), "gray")
+			draw.text((display.width // 2, display.height // 2), "HELLOW WORLD", fill = "white", anchor = "mm")
 		time.sleep(20)
