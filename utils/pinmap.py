c52093bb36420458c301fd66b5ba64cfa13309fc
Kipp Cannon <kipp.cannon@ligo.org>
2023-01-17 14:11:29 +0900

    pinmap.py:  clean up a bit
    - use the programmer as a context manager to automate and ensure power supply shutdown

--- utils/pinmap.py
+++ utils/pinmap.py
@@ -6,45 +6,38 @@ import time
 from tqdm import tqdm
 import tty
 
-programmer = allpro88.allpro88()
-
-print("system ID = 0x%X\nsocket module = %s" % (programmer.system_id, programmer.socket_module.name))
-
-# PCR enable
-programmer.pcr_enable = True
-# set all pins to ground
-for channel in programmer.channel.values():
-	channel.config = allpro88.PINCON.GND
-
 tty_old_settings = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin.fileno())
 
-print("j = pin -, k = pin +, q = quit")
-try:
-	pin = 0
-	with tqdm(total = 87, desc = "blinking pin") as progress:
-		while True:
-			if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
-				c = sys.stdin.read(1)
-				if c == "j":
-					pin = max(0, pin - 1)
-				elif c == "k":
-					pin = min(87, pin + 1)
-				elif c == "q":
-					break
-				progress.n = pin
-				progress.refresh()
-			programmer.channel[pin].config = allpro88.PINCON.LOGICH
-			time.sleep(0.5)
-			programmer.channel[pin].config = allpro88.PINCON.GND
-			time.sleep(0.5)
-except KeyboardInterrupt:
-	pass
+with allpro88.allpro88() as programmer:
+	print("system ID = 0x%X\nsocket module = %s" % (programmer.system_id, programmer.socket_module.name))
+
+	# PCR enable
+	programmer.pcr_enable = True
+	# set all pins to ground
+	for channel in programmer.channel.values():
+		channel.config = allpro88.PINCON.GND
+
+	print("j = channel -, k = channel +, q = quit")
+	try:
+		n = 0
+		with tqdm(total = 87, desc = "blinking channel") as progress:
+			while True:
+				if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+					c = sys.stdin.read(1)
+					if c == "j":
+						n = max(0, n - 1)
+					elif c == "k":
+						n = min(87, n + 1)
+					elif c == "q":
+						break
+					progress.n = n
+					progress.refresh()
+				programmer.channel[n].config = allpro88.PINCON.LOGICH
+				time.sleep(0.5)
+				programmer.channel[n].config = allpro88.PINCON.GND
+				time.sleep(0.5)
+	except KeyboardInterrupt:
+		pass
 
 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, tty_old_settings)
-
-# set all pins to disable
-for channel in programmer.channel.values():
-	channel.config = allpro88.PINCON.DISABLE
-# PCR disable
-programmer.pcr_enable = False
