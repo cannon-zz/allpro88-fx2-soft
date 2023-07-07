3c6a57a2866ed4b8484c0c54e484dfd210837dae
Kipp Cannon <kipp.cannon@ligo.org>
2023-07-07 18:19:19 +0900

    merge ds1230y into m27cxxx

--- utils/m27cxxx.py
+++ utils/m27cxxx.py
@@ -433,6 +433,27 @@ class am29f040(tms28f010):
 	address_bus_pins = (12, 11, 10, 9, 8, 7, 6, 5, 27, 26, 23, 25, 4, 28, 29, 3, 2, 30, 1)
 
 
+class ds1230y(m27cx_width8_program_enable):
+	# NOTE:  this is a battery-backed RAM.  it does not require any
+	# special programming voltages.  it simply requires the
+	# write-enable pin to be set to True (pulled low)
+	socket = "DIP28"
+	voltage_maps = {
+		"read": {
+			14: 0.0,	# GND
+			28: 5.0		# Vcc
+		},
+		"program": {
+			14: 0.0,	# GND
+			28: 5.0		# Vcc
+		}
+	}
+	address_bus_pins = (10, 9, 8, 7, 6, 5, 4, 3, 25, 24, 21, 23, 2, 26, 1)
+	data_bus_pins = (11, 12, 13, 15, 16, 17, 18, 19)
+	chip_enable_pin = 20
+	output_enable_pin = 22
+	program_enable_pin = 27
+
 
 ###
 #
