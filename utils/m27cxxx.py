90abb5bb6605509ab6aa2df544c260441b9e490c
Kipp Cannon <kcannon@cita.utoronto.ca>
2024-04-28 23:15:06 +0900

    m27cxxx.py:  add m27c128

--- utils/m27cxxx.py
+++ utils/m27cxxx.py
@@ -261,6 +261,28 @@ class hn462732(m27c32):
 	Vprog = 25.	# volts
 
 
+class m27c128(m27cx_width8_program_enable):
+	# datasheet for M27128A-2F1
+	socket_name = "DIP28"
+	voltage_maps = {
+		"read": {
+			1: 5.0,		# Vpp
+			14: 0.0,	# GND
+			28: 5.0		# Vcc
+		},
+		"program": {
+			1: 12.5,	# Vpp 12.5 V +/- 0.3 V
+			14: 0.0,	# GND
+			28: 6.25	# Vcc
+		}
+	}
+	address_bus_pins = (10, 9, 8, 7, 6, 5, 4, 3, 25, 24, 21, 23, 2, 26)
+	data_bus_pins = (11, 12, 13, 15, 16, 17, 18, 19)
+	chip_enable_pin = 20
+	output_enable_pin = 22
+	program_enable_pin = 27
+
+
 class m27c256(m27cx_width8_pulse_ce):
 	socket_name = "DIP28"
 	voltage_maps = {
