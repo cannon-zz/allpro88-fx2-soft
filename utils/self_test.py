0842995cb2bbebd35d0d55850f23a8f89ecb1d89
Kipp Cannon <kipp@g.ecc.u-tokyo.ac.jp>
2024-07-30 15:50:15 +0900

    self_test.py test_vpul_ramp():  make a failure more verbose
    - provide a dump of the x and y ramp vectors that failed the test

--- utils/self_test.py
+++ utils/self_test.py
@@ -229,7 +229,7 @@ class channel_driver_test_suite(object):
 		# value?  "agree" = residual < 10 mV.  find the threshold
 		# where this occurs.
 		output_good = abs(model(self.vpul_ramp_x[:32]) - self.vpul_ramp_y[:32]) < 0.01
-		assert any(output_good), "cannot construct VPUL model:  no measured voltages are consistent with fit"
+		assert any(output_good), "cannot construct VPUL model:  no measured voltages are consistent with fit:\nx = %s\ny = %s\nresidual = %s" % (self.vpul_ramp_x[:32], self.vpul_ramp_y[:32], model(self.vpul_ramp_x[:32]) - self.vpul_ramp_y[:32])
 		threshold = max(i for i, val in enumerate(output_good) if not val) + 1
 		assert threshold >= 3
 		#for i in range(32):
