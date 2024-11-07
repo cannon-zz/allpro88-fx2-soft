77bdc28999fc9760a0b9af631a83ae6f0d9bde12
Kipp Cannon <kcannon@cita.utoronto.ca>
2024-11-07 23:45:30 +0900

    allpro88.py:  add a license message

--- utils/allpro88.py
+++ utils/allpro88.py
@@ -1,3 +1,29 @@
+# Copyright (C) 2022-2024  Kipp Cannon
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+#
+# =============================================================================
+#
+#                                   Preamble
+#
+# =============================================================================
+#
+
+
 from enum import IntEnum
 import logging
 import math
@@ -7,15 +33,6 @@ import time
 import usb.core
 import yaml
 from socket_module import socket_modules
-
-
-#
-# =============================================================================
-#
-#                          Environment Configuration
-#
-# =============================================================================
-#
 
 
 logger = logging.getLogger(__name__)
